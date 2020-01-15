cbc3c8a32998889643e9163a5ea4753f9ccaadd7
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2020-01-14 19:50:35 -0600

    Add listeners for resource routes

--- server/__init__.py
+++ server/__init__.py
@@ -5,6 +5,7 @@ import base64
 import cherrypy
 import datetime
 import errno
+import json
 import pathlib
 from operator import itemgetter
 import os
@@ -25,6 +26,7 @@ from girder.models.collection import Collection
 from girder.models.folder import Folder
 from girder.models.upload import Upload
 from girder.utility import RequestBodyStream, assetstore_utilities
+from girder.utility.progress import ProgressContext
 
 
 BUF_SIZE = 65536
@@ -456,6 +458,98 @@ def read_chunk(self, event):
         raise
 
 
+@boundHandler
+@validate_event
+def move_resources(self, event, path):
+    user = self.getCurrentUser()
+    resources = json.loads(event.info["params"]["resources"])
+    progress = event.info["params"]["progress"]
+    total = sum([len(resources[key]) for key in resources])
+    with ProgressContext(
+        progress,
+        user=user,
+        title="Moving resources",
+        message="Calculating requirements...",
+        total=total,
+    ) as ctx:
+        for kind in resources:
+            for obj_id in resources[kind]:
+                source_path = path_from_id(obj_id)
+                ctx.update(message="Moving %s %s" % (kind, source_path.name))
+                shutil.move(
+                    source_path.as_posix(), (path / source_path.name).as_posix()
+                )
+                ctx.update(increment=1)
+    event.preventDefault().addResponse(None)
+
+
+@boundHandler
+@validate_event
+def copy_resources(self, event, path):
+    user = self.getCurrentUser()
+    resources = json.loads(event.info["params"]["resources"])
+    progress = event.info["params"]["progress"]
+    total = sum([len(resources[key]) for key in resources])
+    with ProgressContext(
+        progress,
+        user=user,
+        title="Copying resources",
+        message="Calculating requirements...",
+        total=total,
+    ) as ctx:
+        for kind in resources:
+            for obj_id in resources[kind]:
+                source_path = path_from_id(obj_id)
+                ctx.update(message="Copying %s %s" % (kind, source_path.name))
+                if kind == "folder":
+                    shutil.copytree(
+                        source_path.as_posix(), (path / source_path.name).as_posix()
+                    )
+                else:
+                    shutil.copy(
+                        source_path.as_posix(), (path / source_path.name).as_posix()
+                    )
+                ctx.update(increment=1)
+    event.preventDefault().addResponse(None)
+
+
+@boundHandler
+def delete_resources(self, event):
+    user = self.getCurrentUser()
+    resources = json.loads(event.info["params"]["resources"])
+    remaining_resources = dict(folder=[], item=[])
+    wt_resources = dict(folder=[], item=[])
+    for kind in resources:
+        for obj_id in resources[kind]:
+            if obj_id.startswith("wtlocal:"):
+                wt_resources[kind].append(obj_id)
+            else:
+                remaining_resources[kind].append(obj_id)
+
+    progress = event.info["params"]["progress"]
+    total = sum([len(wt_resources[key]) for key in wt_resources])
+    with ProgressContext(
+        progress,
+        user=user,
+        title="Deleting resources",
+        message="Calculating requirements...",
+        total=total,
+    ) as ctx:
+        for kind in wt_resources:
+            for obj_id in wt_resources[kind]:
+                source_path = path_from_id(obj_id)
+                ctx.update(message="Deleting %s %s" % (kind, source_path.name))
+                if kind == "folder":
+                    shutil.rmtree(source_path.as_posix())
+                else:
+                    source_path.unlink()
+                ctx.update(increment=1)
+    total = sum([len(remaining_resources[key]) for key in remaining_resources])
+    event.info["params"]["resources"] = json.dumps(remaining_resources)
+    if total == 0:
+        event.preventDefault().addResponse(None)
+
+
 def load(info):
     global BASE_COLLECTION, BASE_FOLDER
     BASE_COLLECTION = Collection().createCollection(
@@ -466,7 +560,6 @@ def load(info):
     )
 
     name = "local_objects"
-    # copy resource  'rest.post.resource/copy.before'
     events.bind("rest.get.item.before", name, get_child_items)
     events.bind("rest.post.item.before", name, create_item)
     events.bind("rest.get.item/:id.before", name, get_item_info)
@@ -490,3 +583,7 @@ def load(info):
     events.bind("rest.get.folder/:id.before", name, get_folder_info)
     events.bind("rest.get.folder/:id/rootpath.before", name, folder_root_path)
     events.bind("rest.get.folder/:id/details.before", name, get_folder_details)
+
+    events.bind("rest.delete.resource.before", name, delete_resources)
+    events.bind("rest.post.resource/copy.before", name, copy_resources)
+    events.bind("rest.put.resource/move.before", name, move_resources)
