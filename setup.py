acd200da4a0367b82d1d47cc0939ef92e2be9a5d
Kacper Kowalik (Xarthisius) <xarthisius.kk@gmail.com>
2024-01-19 10:13:33 -0600

    Fix setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="girder-virtual-resources",
     version="1.0.0",
     description="Plugin mapping Girder Folder to physical directories.",
-    packages=["virtual_resources"],
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=["girder"],
     entry_points={
         "girder.plugin": ["virtual_resources = virtual_resources:VirtualResourcesPlugin"]
     },
+    zip_safe=False,
 )
